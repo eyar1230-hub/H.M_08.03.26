ef3d09299545f4897b5683f20dba13167bcf313e
eyar1230-hub <eyar1230@gmail.com>
2026-03-11 15:47:37 +0200

    commit 2

--- exe._bonus.py
+++ exe._bonus.py
@@ -1,1 +1,26 @@
 
+import random
+
+def get_lucky_numbers(amount: int) -> tuple[int]:
+    try:
+        rand_num = []
+        for _ in range(amount):
+            rand_num.append(random.randint(1, 100))
+        return tuple(rand_num)
+    
+
+def input_until_lucky(lucky_numbers: tuple) -> int:
+    count: int = 0
+    while True:
+        guess_lucky: int = int(input('enter your guess: '))
+        count += 1
+        if guess_lucky in lucky_numbers:
+            return count
+
+
+amount = int(input('chose the amount of lucky numbers: '))
+lucky_numbers: tuple = get_lucky_numbers(amount)
+print(input_until_lucky(lucky_numbers))
+print(lucky_numbers)
+
+
--- exe.py
+++ exe.py
@@ -9,17 +9,22 @@ def get_lucky_numbers(amount: int) -> tuple[int]:
 
 
 def input_until_lucky(lucky_numbers: tuple) -> int:
-    count: int = 0
-    while True:
-        guess_lucky: int = int(input('enter your guess: '))
-        count += 1
-        if guess_lucky in lucky_numbers:
-            return count
+    try:
+        count: int = 0
+        while True:
+            guess_lucky: int = int(input('enter your guess: '))
+            count += 1
+            if guess_lucky in lucky_numbers:
+                return count
+    except:
+        input_until_lucky(lucky_numbers)
+        return count
 
 
 amount = int(input('chose the amount of lucky numbers: '))
 lucky_numbers: tuple = get_lucky_numbers(amount)
+print('the lucky numbers are -->', lucky_numbers)
 print(input_until_lucky(lucky_numbers))
-print(lucky_numbers)
+print('the lucky numbers are -->', lucky_numbers)
 
 
